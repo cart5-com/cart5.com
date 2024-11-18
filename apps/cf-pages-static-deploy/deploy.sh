7a3e2531bd158aacb609d30eda5f2c1d46ab2327
kerem <keremtiryaki@gmail.com>
2024-11-17 23:19:23 -0500

    fix: update GIT_HASH log message in index.html

--- apps/cf-pages-static-deploy/deploy.sh
+++ apps/cf-pages-static-deploy/deploy.sh
@@ -2,5 +2,5 @@ echo GIT_HASH=$(git rev-parse HEAD)
 echo "deploying to $CLOUDFLARE_PAGES_TEST_PROJECT_NAME"
 mkdir -p ./dist
 touch ./dist/index.html
-echo "333 hello world GIT_HASH:$GIT_HASH" >> ./dist/index.html
+echo "444 hello world GIT_HASH:$GIT_HASH" >> ./dist/index.html
 wrangler pages deploy dist --project-name=$CLOUDFLARE_PAGES_TEST_PROJECT_NAME --branch=main --no-bundle
