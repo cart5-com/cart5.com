7518f93fe4b0a990ce1ed5315a512bc7d27dc8bc
kerem <keremtiryaki@gmail.com>
2024-11-17 22:22:42 -0500

    update deployment script to modify greeting message in index.html

--- apps/cf-pages-static-deploy/deploy.sh
+++ apps/cf-pages-static-deploy/deploy.sh
@@ -1,4 +1,4 @@
 GIT_HASH=$(git rev-parse HEAD)
-echo "hello world $GIT_HASH" > ./dist/index.html
+echo "222 hello world $GIT_HASH" > ./dist/index.html
 echo "deploying to $CLOUDFLARE_PAGES_TEST_PROJECT_NAME"
 wrangler pages deploy dist --project-name=$CLOUDFLARE_PAGES_TEST_PROJECT_NAME --branch=main --no-bundle
