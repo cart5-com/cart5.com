ea4a5943ae0d4dd85f61f46b9355010a0e9dc627
kerem <keremtiryaki@gmail.com>
2024-11-18 18:55:11 -0500

    debug env var in server

--- apps/auth-hono/src/index.ts
+++ apps/auth-hono/src/index.ts
@@ -7,6 +7,9 @@ app.get('/', (c) => {
   return c.text(`Hello Hono! SAMPLE_ENV_VAR:${process.env.SAMPLE_ENV_VAR}`)
 })
 
+console.log("JSON.stringify(process.env, null, 2)")
+console.log(JSON.stringify(process.env, null, 2))
+
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 console.log(`Server is running on http://localhost:${port}`)
 
