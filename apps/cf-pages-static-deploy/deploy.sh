c0e52ecaefb41c852eb38f409320699773588552
kerem <keremtiryaki@gmail.com>
2024-11-17 22:55:53 -0500

    fix: update deploy script to use local wrangler CLI path

--- apps/cf-pages-static-deploy/deploy.sh
+++ apps/cf-pages-static-deploy/deploy.sh
@@ -1,4 +1,4 @@
 GIT_HASH=$(git rev-parse HEAD)
 echo "222 hello world $GIT_HASH" > ./dist/index.html
 echo "deploying to $CLOUDFLARE_PAGES_TEST_PROJECT_NAME"
-wrangler pages deploy dist --project-name=$CLOUDFLARE_PAGES_TEST_PROJECT_NAME --branch=main --no-bundle
+node ./node_modules/wrangler/wrangler-dist/cli.js pages deploy dist --project-name=$CLOUDFLARE_PAGES_TEST_PROJECT_NAME --branch=main --no-bundle
