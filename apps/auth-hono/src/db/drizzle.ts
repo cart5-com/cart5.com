f9f6445d1a985264b6ceb1b256ccadbfd8af709d
kerem <keremtiryaki@gmail.com>
2024-11-19 16:01:00 -0500

    pnpm add @libsql/linux-x64-gnu

--- apps/auth-hono/src/db/drizzle.ts
+++ apps/auth-hono/src/db/drizzle.ts
@@ -7,17 +7,18 @@ const dbUrl = getEnvironmentVariable("TURSO_AUTH_DB_URL");
 const dbToken = getEnvironmentVariable("TURSO_AUTH_DB_TOKEN");
 
 const db = drizzle({
-	connection: {
-		url: dbUrl,
-		authToken: dbToken
-	}
+    connection: {
+        url: dbUrl,
+        authToken: dbToken
+    }
 });
 
+
 export const checkMigrations = async () => {
-	console.log("Checking migrations...");
-	const __dirname = dirname(new URL(import.meta.url).pathname);
-	await migrate(db, { migrationsFolder: resolve(__dirname, "./sql-gen") });
-	console.log("Migrations checked");
+    console.log("Checking migrations...");
+    const __dirname = dirname(new URL(import.meta.url).pathname);
+    await migrate(db, { migrationsFolder: resolve(__dirname, "./sql-gen") });
+    console.log("Migrations checked");
 };
 
 export default db;
