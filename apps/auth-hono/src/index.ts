65e4f09632754b6c6dacff13fa8cf38b0b27253e
kerem <keremtiryaki@gmail.com>
2024-11-18 23:45:38 -0500

    fix: improve logging of environment variables in the index file

--- apps/auth-hono/src/index.ts
+++ apps/auth-hono/src/index.ts
@@ -7,8 +7,10 @@ app.get('/', (c) => {
   return c.text(`Hello Hono! SAMPLE_ENV_VAR:${process.env.SAMPLE_ENV_VAR}`)
 })
 
-console.log("JSON.stringify(process.env, null, 2)")
-console.log(JSON.stringify(process.env, null, 2))
+console.log(JSON.stringify("process.env.PORT", null, 2))
+console.log(JSON.stringify(process.env.PORT, null, 2))
+console.log("JSON.stringify(process.env.SAMPLE_ENV_VAR, null, 2)")
+console.log(JSON.stringify(process.env.SAMPLE_ENV_VAR, null, 2))
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 console.log(`Server is running on http://localhost:${port}`)
