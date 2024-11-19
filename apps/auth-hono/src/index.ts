eac95f02bfcba330062fa6facbf2bf622f7e71ca
kerem <keremtiryaki@gmail.com>
2024-11-19 13:04:31 -0500

    fix: update environment variable handling and improve configuration clarity

--- apps/auth-hono/src/index.ts
+++ apps/auth-hono/src/index.ts
@@ -4,13 +4,16 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text(`Hello Hono! SAMPLE_ENV_VAR:${process.env.SAMPLE_ENV_VAR}`)
-})
+  return c.text(`
+    Hello Hono! SAMPLE_ENV_VAR:${process.env.SAMPLE_ENV_VAR}
 
-console.log(JSON.stringify("process.env.PORT", null, 2))
-console.log(JSON.stringify(process.env.PORT, null, 2))
-console.log("JSON.stringify(process.env.SAMPLE_ENV_VAR, null, 2)")
-console.log(JSON.stringify(process.env.SAMPLE_ENV_VAR, null, 2))
+    PORT:${process.env.PORT}
+
+    NODE_ENV:${process.env.NODE_ENV}
+
+    SAMPLE_NIXPACKS_VAR:${process.env.SAMPLE_NIXPACKS_VAR}
+`)
+})
 
 const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
 console.log(`Server is running on http://localhost:${port}`)
