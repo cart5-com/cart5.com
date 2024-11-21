36dc35b308131fb31b99254526599afade9fd693
kerem <keremtiryaki@gmail.com>
2024-11-21 12:22:44 -0500

    chore: fix formatting in dev.nix by correcting URLs for documentation links

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,14 +1,14 @@
 # To learn more about how to use Nix to configure your environment
-# see: https: //developers.google.com/idx/guides/customize-idx-env
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ...}: {
     # Which nixpkgs channel to use.
     channel = "stable-24.05"; # or "unstable"
-    # Use https: //search.nixos.org/packages to find packages
+    # Use https://search.nixos.org/packages to find packages
     packages = [ pkgs.nodejs_20 pkgs.pnpm pkgs.xcaddy pkgs.turso-cli pkgs.ansible ];
     # Sets environment variables in the workspace
     env = {};
     idx = {
-        # Search for the extensions you want on https: //open-vsx.org/ and use "publisher.id"
+        # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
         extensions = ["astro-build.astro-vscode" "Vue.volar"];
         workspace = {
             # Runs when a workspace is first created with this `dev.nix` file
