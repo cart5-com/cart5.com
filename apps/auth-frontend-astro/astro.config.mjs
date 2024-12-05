a4c67b2a648e7415a0965894ab820542cb31a67e
kerem <keremtiryaki@gmail.com>
2024-12-05 00:23:00 -0500

    auth-frontend-astro: add Tailwind CSS integration

--- apps/auth-frontend-astro/astro.config.mjs
+++ apps/auth-frontend-astro/astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import vue from "@astrojs/vue";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -13,5 +15,5 @@ export default defineConfig({
       }
   },
 
-  integrations: [vue()]
+  integrations: [vue(), tailwind()]
 });
