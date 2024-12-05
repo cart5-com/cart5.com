99e3a126de35406d409b1cb3046f291add711e94
kerem <keremtiryaki@gmail.com>
2024-12-05 00:21:13 -0500

    auth-frontend-astro: add Vue integration

--- apps/auth-frontend-astro/astro.config.mjs
+++ apps/auth-frontend-astro/astro.config.mjs
@@ -1,13 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        envDir: "../../",
-        build: {
-            // sourcemap: true,
-            minify: false,
-        }
-    }
+  vite: {
+      envDir: "../../",
+      build: {
+          // sourcemap: true,
+          minify: false,
+      }
+  },
+
+  integrations: [vue()]
 });
