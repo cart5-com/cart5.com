63ff83a811fc2108a3b7baaf5ed753bb94b4c17b
kerem <keremtiryaki@gmail.com>
2024-12-05 01:29:21 -0500

    add '@node-rs/argon2' to external dependencies in esbuild configuration

--- apps/auth-api-hono/esbuild.cjs
+++ apps/auth-api-hono/esbuild.cjs
@@ -8,7 +8,11 @@ require("esbuild").build({
     minify: true,
     sourcemap: true,
     plugins: [],
-    external: [],
+    external: [
+        // TODO: need to make sure these dependencies are copied to your dist folder. 
+        // add a copy step to your build script in package.json: `build && cp -r node_modules/@node-rs dist/`
+        '@node-rs/argon2'
+    ],
     banner: {
         js: `
             import { createRequire } from 'module';
