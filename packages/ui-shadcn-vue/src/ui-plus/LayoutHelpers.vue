39644ddb36c4698b281cd5927a2271494563c9d5
kerem <keremtiryaki@gmail.com>
2024-12-17 19:07:23 -0500

    Easy Sonner/Toast

---
+++ packages/ui-shadcn-vue/src/ui-plus/LayoutHelpers.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { useColorMode } from "@vueuse/core";
+import DialogProvider from "@/ui-plus/dialog/DialogProvider.vue";
+import { Toaster } from "@/components/ui/sonner";
+import { computed } from "vue";
+// import DarkModeToggle from "@/ui-plus/DarkModeToggle.vue";
+
+const mode = useColorMode();
+const toasterTheme = computed(() => (mode.value === "dark" ? "dark" : "light"));
+</script>
+
+<template>
+    <!-- <DarkModeToggle /> -->
+    <DialogProvider />
+    <Toaster :theme="toasterTheme"
+             closeButton
+             richColors
+             class="pointer-events-auto" />
+</template>
