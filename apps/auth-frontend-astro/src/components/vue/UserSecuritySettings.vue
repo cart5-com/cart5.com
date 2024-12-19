5235292869501329e1fc3f9d10fd4c8dd91e20d6
kerem <keremtiryaki@gmail.com>
2024-12-19 13:31:16 -0500

    refactor: enhance User Security Settings layout with collapsible details and improved messaging for session requirements

--- apps/auth-frontend-astro/src/components/vue/UserSecuritySettings.vue
+++ apps/auth-frontend-astro/src/components/vue/UserSecuritySettings.vue
@@ -9,23 +9,26 @@ const user = useStore($userStore);
 
 <template>
     <div>
-        <h2 class="text-lg font-bold flex items-center gap-2">
-            <ShieldCheck stroke-width="3" />
-            Security
-        </h2>
-        <div v-if="!user?.hasNewSession"
-             class="text-destructive text-sm my-2">
-            Fresh session required.
+        <details class="mb-4 px-4">
+            <summary class="cursor-pointer w-full flex items-center gap-2 bg-muted p-2 rounded-md">
+                <ShieldCheck stroke-width="3" /> Security
+            </summary>
             <div>
-                Please log out and log in again to change your security settings
+                <div v-if="!user?.hasNewSession"
+                     class="text-destructive text-sm my-2">
+                    Fresh session required.
+                    <div>
+                        Please log out and log in again to change your security settings
+                    </div>
+                    <Separator class="my-10" />
+                </div>
+                <Setup2FAButton class="my-10"
+                                client:only="vue" />
+                <Separator class="my-10" />
+                <!-- TODO: update password button -->
+                <!-- <Separator class="my-10" /> -->
+                <!-- TODO: update name button -->
             </div>
-            <Separator class="my-10" />
-        </div>
-        <Setup2FAButton class="my-10"
-                        client:only="vue" />
-        <Separator class="my-10" />
-        <!-- TODO: update password button -->
-        <!-- <Separator class="my-10" /> -->
-        <!-- TODO: update name button -->
+        </details>
     </div>
 </template>
