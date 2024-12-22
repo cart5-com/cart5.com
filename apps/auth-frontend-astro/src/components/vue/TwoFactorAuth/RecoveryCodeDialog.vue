c45dec9fe2117917a47f9b7be0b28ca7071ff4f6
kerem <keremtiryaki@gmail.com>
2024-12-21 22:49:09 -0500

    ui-auth: enhance RecoveryCodeDialog with security instructions

--- apps/auth-frontend-astro/src/components/vue/TwoFactorAuth/RecoveryCodeDialog.vue
+++ apps/auth-frontend-astro/src/components/vue/TwoFactorAuth/RecoveryCodeDialog.vue
@@ -9,11 +9,20 @@ const props = defineProps<{
 </script>
 
 <template>
-    <div class="flex items-center space-x-2 my-6">
-        <Input type="text"
-               disabled
-               v-model="props.recoveryCode"
-               class="flex-1" />
-        <CopyButton :content="props.recoveryCode" />
+    <div>
+        <h2 class="text-2xl font-bold">Your account is secured</h2>
+
+        <p class="text-sm">
+            Write down this emergency backup code and store it somewhere safe.
+            If your device is ever lost or stolen, you can use this code to unlock your account.
+        </p>
+
+        <div class="flex items-center space-x-2 my-6">
+            <Input type="text"
+                   disabled
+                   v-model="props.recoveryCode"
+                   class="flex-1" />
+            <CopyButton :content="props.recoveryCode" />
+        </div>
     </div>
 </template>
