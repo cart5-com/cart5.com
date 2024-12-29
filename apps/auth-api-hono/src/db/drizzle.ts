2ec72a1d69fecf46c631cfd87030ed97763f5ef3
kerem <keremtiryaki@gmail.com>
2024-12-29 01:12:09 -0500

    PROD: use Turso database for auth-api

--- apps/auth-api-hono/src/db/drizzle.ts
+++ apps/auth-api-hono/src/db/drizzle.ts
@@ -1,9 +1,19 @@
 import { drizzle } from "drizzle-orm/libsql";
 import { localDbPath } from "../consts";
 import { checkMigrations } from "./migrate";
+import { IS_PROD } from "../utils/getEnvironmentVariable";
 
-const db: ReturnType<typeof drizzle> = drizzle(localDbPath);
+const db: ReturnType<typeof drizzle> = IS_PROD ?
+    // PROD
+    drizzle({
+        connection: {
+            url: process.env.AUTHAPI_TURSO_DB_URL!,
+            authToken: process.env.AUTHAPI_TURSO_DB_TOKEN!
+        }
+    })
+    :
+    // DEV
+    drizzle(localDbPath);
 export default db;
-
 
 checkMigrations();
--- scripts/deploy/_start.sh
+++ scripts/deploy/_start.sh
@@ -1,2 +1,3 @@
 # TODO: test migrations with turso branching before deploying
 ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ./_0_common-role.yml -v
+# lightsail # ansible-playbook -i "$UBUNTU_SERVER_IP," -u ubuntu --private-key ./key.pem ./_0_common-role.yml -v
