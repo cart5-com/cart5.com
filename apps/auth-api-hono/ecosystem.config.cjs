15e5df701b05291a2a8ac0b56a996ff47c86f7c2
kerem <keremtiryaki@gmail.com>
2024-12-29 23:53:20 -0500

    feat: update environment configuration to use .env.production and add dotenv dependency

--- apps/auth-api-hono/ecosystem.config.cjs
+++ apps/auth-api-hono/ecosystem.config.cjs
@@ -2,7 +2,7 @@
 // var envConfig = require('dotenv').config({ path: penv });
 const path = require('path');
 require('dotenv').config({
-    path: path.join(__dirname, '../../.env')
+    path: path.join(__dirname, '../../.env.production')
 });
 
 module.exports = {
