a6272a91751ea5d29254c93ec80ae013bafc3432
kerem <keremtiryaki@gmail.com>
2024-12-29 23:49:55 -0500

    feat: add deployment roles for auth-api-hono and auth-frontend-astro, update Caddyfile and start script

---
+++ apps/auth-api-hono/ecosystem.config.cjs
@@ -0,0 +1,20 @@
+// var penv = require('path').join(__dirname, "./.env");
+// var envConfig = require('dotenv').config({ path: penv });
+const path = require('path');
+require('dotenv').config({
+    path: path.join(__dirname, '../../.env')
+});
+
+module.exports = {
+    apps: [
+        {
+            name: "auth-api-hono",
+            script: "./dist/index.js",
+            // // cron_restart: "0 */6 * * *", // every 6 hours
+            // env: Object.assign({}, envConfig, {
+            //     "HOST": "0.0.0.0",
+            //     "PORT": "3002"
+            // }),
+        },
+    ],
+}
--- scripts/deploy/_start.sh
+++ scripts/deploy/_start.sh
@@ -1,4 +1,6 @@
 # TODO: test migrations with turso branching before deploying
-ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ./_0_common-role.yml -v
-ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ./_1_caddy-role.yml -v
+ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ./common-role.yml -v 
+ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ../../apps/auth-api-hono/deploy/auth-api-hono-deploy-role.yml -v
+ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ../../apps/auth-frontend-astro/deploy/auth-frontend-astro-deploy-role.yml -v
+ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ../../apps/proxy-caddy/deploy/caddy-deploy-role.yml -v
 # lightsail # ansible-playbook -i "$UBUNTU_SERVER_IP," -u ubuntu --private-key ./key.pem ./_0_common-role.yml -v
