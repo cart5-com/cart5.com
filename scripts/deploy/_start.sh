f1d04812c6b6294d31d9ea64b2abec632f5a196a
kerem <keremtiryaki@gmail.com>
2025-01-02 01:11:36 -0500

    refactor: update deployment script comments for clarity on AWS Lightsail usage

--- scripts/deploy/_start.sh
+++ scripts/deploy/_start.sh
@@ -1,4 +1,4 @@
 # TODO: test migrations with turso branching before deploying
 ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ./common-role.yml -v 
-# lightsail # ansible-playbook -i "$UBUNTU_SERVER_IP," -u ubuntu --private-key ./key.pem ./common-role.yml -v
+# AWS lightsail # ansible-playbook -i "$UBUNTU_SERVER_IP," -u ubuntu --private-key ./key.pem ./common-role.yml -v
 ansible-playbook -i "$UBUNTU_SERVER_IP," -u root ../../apps/proxy-caddy/deploy/caddy-deploy-role.yml -v
