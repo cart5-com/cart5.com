4494584e161df44a5473ce6fb196e2ff297dfa7e
kerem <keremtiryaki@gmail.com>
2025-01-05 02:37:37 -0500

    feat: implement embedded database support in drizzle.ts for production environment

--- apps/auth-api-hono/src/db/drizzle.ts
+++ apps/auth-api-hono/src/db/drizzle.ts
@@ -3,6 +3,8 @@ import { localDbPath } from "lib/auth-consts";
 import type { honoTypes } from "..";
 import type { Context } from "hono";
 import { env } from "hono/adapter";
+import { createClient } from '@libsql/client';
+
 
 let db: ReturnType<typeof drizzle>;
 export const getDrizzleDb = function (c: Context<honoTypes>): ReturnType<typeof drizzle> {
@@ -12,33 +14,32 @@ export const getDrizzleDb = function (c: Context<honoTypes>): ReturnType<typeof 
     const {
         AUTHAPI_TURSO_DB_URL,
         AUTHAPI_TURSO_DB_TOKEN,
-        // AUTHAPI_TURSO_EMBEDDED_DB_PATH
+        AUTHAPI_TURSO_EMBEDDED_DB_PATH
     } = env(c);
     const IS_PROD = c.get('IS_PROD')
     if (IS_PROD) {
-        // if (AUTHAPI_TURSO_EMBEDDED_DB_PATH) {
-        //     // has embedded db
-        //     console.log("🔥🔥🔥has embedded db🔥🔥🔥");
-        //     db = drizzle({
-        //         connection: {
-        //             url: `file:${AUTHAPI_TURSO_EMBEDDED_DB_PATH}`,
-        //             authToken: AUTHAPI_TURSO_DB_TOKEN!,
-        //             syncUrl: AUTHAPI_TURSO_DB_URL!,
-        //             syncInterval: 120,
-        //         }
-        //     })
-        //     return db;
-        // } else {
-        // no embedded db
-        console.log("🔥🔥🔥no embedded db🔥🔥🔥");
-        db = drizzle({
-            connection: {
-                url: AUTHAPI_TURSO_DB_URL!,
+        if (AUTHAPI_TURSO_EMBEDDED_DB_PATH) {
+            // has embedded db
+            console.log("🔥🔥🔥has embedded db🔥🔥🔥");
+            const client = createClient({
+                url: `file:${AUTHAPI_TURSO_EMBEDDED_DB_PATH}`,
                 authToken: AUTHAPI_TURSO_DB_TOKEN!,
-            }
-        })
-        return db;
-        // }
+                syncUrl: AUTHAPI_TURSO_DB_URL!,
+                syncInterval: 120,
+            });
+            const db = drizzle(client);
+            return db;
+        } else {
+            // no embedded db
+            console.log("🔥🔥🔥no embedded db🔥🔥🔥");
+            db = drizzle({
+                connection: {
+                    url: AUTHAPI_TURSO_DB_URL!,
+                    authToken: AUTHAPI_TURSO_DB_TOKEN!,
+                }
+            })
+            return db;
+        }
     } else {
         // DEV
         console.log("🔥🔥🔥DEV🔥🔥🔥");
