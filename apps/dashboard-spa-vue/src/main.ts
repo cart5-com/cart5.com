3182045afde83ee20c01d64395000201f2f1862d
kerem <keremtiryaki@gmail.com>
2025-01-10 15:44:06 -0500

    Refactor user authentication logic to handle errors and improve user redirection

--- apps/dashboard-spa-vue/src/main.ts
+++ apps/dashboard-spa-vue/src/main.ts
@@ -6,15 +6,24 @@ import { createAuthApiClient } from 'lib/apiClients/authApiClient';
 import router from './router'
 
 export const getUser = async function () {
-    window.USER = (await (await createAuthApiClient().api.user.whoami.$post()).json()).data;
-    if (!window.USER) {
-        window.location.href = getLoginUrl(import.meta.env.VITE_PUBLIC_DOMAIN_NAME);
+    const { data, error } = await (await createAuthApiClient().api.user.whoami.$post()).json();
+    if (error) {
+        console.error(error);
     }
-    return window.USER;
+    if (data === null) {
+        window.location.href = getLoginUrl(import.meta.env.VITE_PUBLIC_DOMAIN_NAME);
+        return null;
+    } else {
+        return data;
+    }
 };
 
-getUser().then(() => {
-    const app = createApp(App);
-    app.use(router);
-    app.mount('#app');
-});
+getUser()
+    .then((user) => {
+        if (user) {
+            window.USER = user;
+            const app = createApp(App);
+            app.use(router);
+            app.mount('#app');
+        }
+    });
