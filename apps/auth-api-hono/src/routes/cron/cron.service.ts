1d9e3ddf998805d66e08539b995910dba0de262b
kerem <keremtiryaki@gmail.com>
2025-01-12 15:46:13 -0500

    refactor: rename deleteExpiredSessions to deleteExpiredSessionsService and update import paths

--- apps/auth-api-hono/src/routes/cron/cron.service.ts
+++ apps/auth-api-hono/src/routes/cron/cron.service.ts
@@ -1,9 +1,9 @@
 import { lte } from "drizzle-orm";
-import { sessionTable } from "../schema";
-import db from "../drizzle";
+import { sessionTable } from "../../db/schema";
+import db from "../../db/drizzle";
 
 // TODO: add a cron job to delete expired sessions
-export async function deleteExpiredSessions() {
+export const deleteExpiredSessionsService = async () => {
     return (
         await db
             .delete(sessionTable)
