206d9b0d3b5abe3b8605964caf2290e10c94aa3f
kerem <keremtiryaki@gmail.com>
2025-01-14 17:06:17 -0500

    update: add Autocomplete component and Sample

--- apps/dashboard-spa-vue/src/pages/restaurant/AddressView.vue
+++ apps/dashboard-spa-vue/src/pages/restaurant/AddressView.vue
@@ -1,10 +1,14 @@
 <script lang="ts" setup>
 import { currentRestaurant } from '@src/stores/RestaurantStore';
+import Sample from '@/ui-plus/auto-complete/Sample.vue';
 </script>
 
 <template>
     <div>
         <h1>Restaurant Address</h1>
         {{ currentRestaurant?.name }}
+    </div>
+    <div>
+        <Sample />
     </div>
 </template>
