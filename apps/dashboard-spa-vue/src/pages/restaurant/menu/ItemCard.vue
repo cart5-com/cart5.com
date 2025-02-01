cefddaf0431588d2f432d7c96e66f9f024943f30
kerem <keremtiryaki@gmail.com>
2025-02-01 16:57:41 -0500

    feat: add ItemCard and MenuTab components for enhanced menu management and item display

---
+++ apps/dashboard-spa-vue/src/pages/restaurant/menu/ItemCard.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+import { MenuJSON } from "lib/types/menuTypes";
+defineProps<{
+    menuJSON: MenuJSON,
+    itemId: string
+}>()
+</script>
+
+<template>
+    <div class="flex bg-card rounded-lg overflow-hidden border">
+        <Card class="flex-grow border-none">
+            <CardHeader>
+                <CardTitle class="flex items-center gap-2">
+                    <div class="item-drag-handle cursor-grab active:cursor-grabbing"
+                         style="touch-action: none">
+                        <MoveIcon />
+                    </div>
+                    <h1 class="text-lg font-bold">
+                        {{ menuJSON?.allItems?.[itemId]?.itemLabel }}
+                    </h1>
+                    <!-- <Input :model-value="itemsMap.get(itemId)?.itemLabel"
+                                                       @update:model-value="(
+                                                        newValue: string | number
+                                                    ) => {
+                                                        const item = itemsMap.get(itemId);
+                                                        if (item) {
+                                                            item.itemLabel = newValue.toString();
+                                                        }
+                                                    }"
+                                                       type="text" /> -->
+                </CardTitle>
+                <CardDescription>
+                    ${{ menuJSON?.allItems?.[itemId]?.price?.toFixed(2) }}
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <p class="text-xs line-clamp-3">
+                    {{ menuJSON?.allItems?.[itemId]?.description }}
+                </p>
+            </CardContent>
+            <CardFooter>
+                <!-- <Button class="w-full">Add to Cart</Button> -->
+            </CardFooter>
+        </Card>
+
+        <div class="flex-shrink-0"
+             v-if="menuJSON?.allItems?.[itemId]?.imageUrl">
+            <div class="flex items-center justify-center h-full">
+                <img :src="menuJSON?.allItems?.[itemId]?.imageUrl"
+                     :alt="menuJSON?.allItems?.[itemId]?.itemLabel"
+                     loading="lazy"
+                     class="h-full w-48 object-cover overflow-hidden" />
+            </div>
+        </div>
+    </div>
+</template>
--- apps/dashboard-spa-vue/src/pages/restaurant/menu/MenuEditorView.vue
+++ apps/dashboard-spa-vue/src/pages/restaurant/menu/MenuEditorView.vue
@@ -2,31 +2,26 @@
 import { onMounted, ref, watch } from "vue";
 import { pageTitle } from '@src/stores/layout.store'
 import {
-    Card,
-    CardHeader,
-    CardTitle,
-    CardDescription,
-    CardContent,
-    CardFooter
-} from "@/components/ui/card";
-import { AlignJustify, Check, ChevronDown, ChevronUp, Loader2, MoveIcon, Pencil, Plus } from "lucide-vue-next";
-import { Badge } from '@/components/ui/badge'
-
-import draggable from "vuedraggable";
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from '@/components/ui/tabs'
+import {
+    Check,
+    Loader2,
+} from "lucide-vue-next";
 import CategoryDialog from "./CategoryDialog.vue";
 import { Button } from "@/components/ui/button";
-import { WeeklyScheduleAsString } from "lib/types/menuTypes";
-
-
-pageTitle.value = 'Menu Editor'
-const isLoading = ref(false);
-const showCategories = ref(true);
-const categoryDialog = ref<InstanceType<typeof CategoryDialog>>();
-
 import type { MenuJSON } from "lib/types/menuTypes";
 import { dashboardApiClient } from "@src/lib/dashboardApiClient";
 import { currentRestaurantId } from "@src/stores/RestaurantStore";
 import { toast } from '@/ui-plus/sonner';
+import MenuTab from "./MenuTab.vue";
+
+pageTitle.value = 'Menu Editor'
+const isLoading = ref(false);
+const categoryDialog = ref<InstanceType<typeof CategoryDialog>>();
 
 const currentCategoryId = ref<string | null>(null);
 
@@ -152,123 +147,53 @@ const addNewCategory = () => {
 
 <template>
     <div class="p-0 sm:p-2">
-        <Button @click="saveMenu()"
-                variant="secondary"
-                :disabled="isLoading">
-            <Loader2 class="w-4 h-4 animate-spin"
-                     v-if="isLoading" />Auto save
-            <Check />
-        </Button>
         <div class="mx-auto mt-4">
             <CategoryDialog ref="categoryDialog"
                             v-if="currentCategoryId"
                             :category="menuJSON?.allCategories?.[currentCategoryId]" />
-            <draggable v-if="menuJSON"
-                       v-model="menuJSON.categoryIdsOrder"
-                       group="categories"
-                       handle=".cat-drag-handle"
-                       item-key="catId">
-                <template #item="{ element: categoryId }">
-                    <div class="category-container border rounded-lg mb-12 bg-background">
-                        <div class="flex justify-between items-center border-b border-muted-foreground p-2 ">
-                            <h1 class="text-2xl font-bold">
-                                {{ menuJSON?.allCategories?.[categoryId]?.categoryLabel }}
-                                <Badge v-if="
-                                    menuJSON?.allCategories?.[categoryId]?.isLimitedTime?.type === 'always' &&
-                                    menuJSON?.allCategories?.[categoryId]?.isLimitedTime?.alwaysValue
-                                "
-                                       variant="outline">
-                                    Always
-                                </Badge>
-                                <Badge class="line-clamp-1"
-                                       :style="{ maxWidth: '10ch' }"
-                                       v-if="
-                                        menuJSON?.allCategories?.[categoryId]?.isLimitedTime?.type === 'weeklySchedule' &&
-                                        menuJSON?.allCategories?.[categoryId]?.isLimitedTime?.weeklyScheduleValue
-                                    "
-                                       variant="outline">
-                                    {{ WeeklyScheduleAsString(menuJSON?.allCategories?.[categoryId]?.isLimitedTime?.weeklyScheduleValue) }}
-                                </Badge>
-                            </h1>
-                            <div class="flex items-center gap-2">
-                                <AlignJustify class="cursor-pointer cat-drag-handle" />
-                                <Pencil class="cursor-pointer"
-                                        @click="openCategoryDialog(categoryId)" />
-                                <div class="cursor-pointer"
-                                     @click="showCategories = !showCategories">
-                                    <ChevronUp v-if="showCategories" />
-                                    <ChevronDown v-else />
-                                </div>
-                            </div>
-                        </div>
-                        <draggable v-if="showCategories && menuJSON && menuJSON.allCategories"
-                                   v-model="menuJSON.allCategories[categoryId].itemIds"
-                                   group="items"
-                                   item-key="itemId"
-                                   handle=".item-drag-handle"
-                                   class="grid grid-cols-1 gap-6 lg:grid-cols-2  p-2 mt-4">
-                            <template #item="{ element: itemId }">
-                                <div class="flex bg-card rounded-lg overflow-hidden border">
-                                    <Card class="flex-grow border-none">
-                                        <CardHeader>
-                                            <CardTitle class="flex items-center gap-2">
-                                                <div class="item-drag-handle cursor-grab active:cursor-grabbing"
-                                                     style="touch-action: none">
-                                                    <MoveIcon />
-                                                </div>
-                                                <h1 class="text-lg font-bold">
-                                                    {{ menuJSON?.allItems?.[itemId]?.itemLabel }}
-                                                </h1>
-                                                <!-- <Input :model-value="itemsMap.get(itemId)?.itemLabel"
-                                                       @update:model-value="(
-                                                        newValue: string | number
-                                                    ) => {
-                                                        const item = itemsMap.get(itemId);
-                                                        if (item) {
-                                                            item.itemLabel = newValue.toString();
-                                                        }
-                                                    }"
-                                                       type="text" /> -->
-                                            </CardTitle>
-                                            <CardDescription>
-                                                ${{ menuJSON?.allItems?.[itemId]?.price?.toFixed(2) }}
-                                            </CardDescription>
-                                        </CardHeader>
-                                        <CardContent>
-                                            <p class="text-xs line-clamp-3">
-                                                {{ menuJSON?.allItems?.[itemId]?.description }}
-                                            </p>
-                                        </CardContent>
-                                        <CardFooter>
-                                            <!-- <Button class="w-full">Add to Cart</Button> -->
-                                        </CardFooter>
-                                    </Card>
 
-                                    <div class="flex-shrink-0"
-                                         v-if="menuJSON?.allItems?.[itemId]?.imageUrl">
-                                        <div class="flex items-center justify-center h-full">
-                                            <img :src="menuJSON?.allItems?.[itemId]?.imageUrl"
-                                                 :alt="menuJSON?.allItems?.[itemId]?.itemLabel"
-                                                 loading="lazy"
-                                                 class="h-full w-48 object-cover overflow-hidden" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </template>
-                        </draggable>
-                    </div>
-                </template>
-            </draggable>
-            <Button class="my-4"
-                    variant="outline"
-                    @click="addNewCategory">
-                <Plus /> Add New Category
+
+
+            <Tabs default-value="menu"
+                  class="w-full">
+                <TabsList class="">
+                    <TabsTrigger value="menu">
+                        Menu
+                    </TabsTrigger>
+                    <TabsTrigger value="items">
+                        Items
+                    </TabsTrigger>
+                    <TabsTrigger value="optionGroups">
+                        Choices & Addons
+                    </TabsTrigger>
+                </TabsList>
+                <TabsContent value="menu">
+                    <MenuTab :menuJSON="menuJSON"
+                             :openCategoryDialog="openCategoryDialog"
+                             :addNewCategory="addNewCategory" />
+                </TabsContent>
+                <TabsContent value="items">
+                    <div>Items</div>
+                </TabsContent>
+                <TabsContent value="optionGroups">
+                    <div>Choices & Addons</div>
+                </TabsContent>
+            </Tabs>
+
+
+            <Button @click="saveMenu()"
+                    variant="secondary"
+                    class="mt-4"
+                    :disabled="isLoading">
+                <Loader2 class="w-4 h-4 animate-spin"
+                         v-if="isLoading" />Auto save
+                <Check />
             </Button>
         </div>
-        <div>
+        <div class="mt-4">
             <details>
                 <summary>menuJSON</summary>
-                <pre>{{ menuJSON }}</pre>
+                <pre class="text-xs max-h-96 overflow-y-auto">{{ menuJSON }}</pre>
             </details>
         </div>
     </div>
