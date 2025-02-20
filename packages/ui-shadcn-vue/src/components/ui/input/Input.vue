547473428cc5ec59e0a797700ccbd1dbcb921206
kerem <keremtiryaki@gmail.com>
2025-02-20 16:38:44 -0500

    feat: update Input component to remove File type from modelValue and defaultValue props

--- packages/ui-shadcn-vue/src/components/ui/input/Input.vue
+++ packages/ui-shadcn-vue/src/components/ui/input/Input.vue
@@ -4,13 +4,13 @@ import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
 
 const props = defineProps<{
-  defaultValue?: File | string | number
-  modelValue?: File | string | number
+  defaultValue?: string | number
+  modelValue?: string | number
   class?: HTMLAttributes['class']
 }>()
 
 const emits = defineEmits<{
-  (e: 'update:modelValue', payload: File | string | number): void
+  (e: 'update:modelValue', payload: string | number): void
 }>()
 
 const modelValue = useVModel(props, 'modelValue', emits, {
