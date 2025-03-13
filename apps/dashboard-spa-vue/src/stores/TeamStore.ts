320791fa5cd4339ed13f53dbe91938e776ce0817
kerem <keremtiryaki@gmail.com>
2025-03-13 15:06:18 -0400

    fix: update alert message for domain redirection in TeamStore to improve clarity

--- apps/dashboard-spa-vue/src/stores/TeamStore.ts
+++ apps/dashboard-spa-vue/src/stores/TeamStore.ts
@@ -14,7 +14,7 @@ export async function loadMyTeams() {
     const response = await (await dashboardApiClient.api_dashboard.team.my_teams.$get()).json()
     myTeams.value = response.data
     if (window.location.host !== myTeams.value.hostnameTeam?.defaultHostname) {
-        alert('Redirecting to default website')
+        alert('Redirecting to default domain')
         window.location.href = window.location.href.replace(window.location.host, myTeams.value.hostnameTeam?.defaultHostname!)
     }
     console.log(myTeams.value)
