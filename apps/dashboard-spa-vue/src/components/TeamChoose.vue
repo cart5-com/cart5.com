c126213b1de4971ba8aa5c173a1459db87bd3f73
kerem <keremtiryaki@gmail.com>
2025-03-13 16:58:54 -0400

    fix: update hostname redirection logic in TeamChoose component to ensure correct navigation

--- apps/dashboard-spa-vue/src/components/TeamChoose.vue
+++ apps/dashboard-spa-vue/src/components/TeamChoose.vue
@@ -12,7 +12,8 @@ const open = ref(false);
 const onTeamSelected = (team: MyTeamsType['myTeams'][number]) => {
     if (team.defaultHostname) {
         if (window.location.host !== team.defaultHostname) {
-            window.location.href = `https://${team.defaultHostname}/dash/`;
+            // window.location.href = `https://${team.defaultHostname}/dash/`;
+            window.location.href = window.location.href.replace(window.location.host, team.defaultHostname);
         }
     } else {
         console.log('no default hostname', team);
