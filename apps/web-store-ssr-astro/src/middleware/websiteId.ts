fba007d6ce3d307b65617fd8440a49dd1e68d940
kerem <keremtiryaki@gmail.com>
2025-03-14 21:07:32 -0400

    chore: update environment variables for AUTHAPI_TURSO_DB_URL and AUTHAPI_TURSO_DB_TOKEN, and re-enable websiteIdMiddleware for request handling

--- apps/web-store-ssr-astro/src/middleware/websiteId.ts
+++ apps/web-store-ssr-astro/src/middleware/websiteId.ts
@@ -1,21 +1,20 @@
 import { defineMiddleware } from "astro:middleware";
 import { getWebsiteByDefaultHostname, getRedirectHostname } from "lib/db/dbUtils/getWebsiteByDefaultHostname";
-import { IS_PROD } from "lib/utils/getEnvVariable";
 
 export const websiteIdMiddleware = defineMiddleware(async (context, next) => {
     const host = context.request.headers.get("Host");
-    // context.locals.WEBSITE = await getWebsiteByDefaultHostname(host ?? '');
-    // if (!context.locals.WEBSITE) {
-    //     const redirectHostname = await getRedirectHostname(host ?? '');
-    //     if (redirectHostname) {
-    //         return context.redirect(
-    //             context.request.url.replace(host ?? '', redirectHostname)
-    //         );
-    //     } else {
-    //         return context.redirect(
-    //             context.request.url.replace(host ?? '', import.meta.env.PUBLIC_DOMAIN_NAME)
-    //         );
-    //     }
-    // }
+    context.locals.WEBSITE = await getWebsiteByDefaultHostname(host ?? '');
+    if (!context.locals.WEBSITE) {
+        const redirectHostname = await getRedirectHostname(host ?? '');
+        if (redirectHostname) {
+            return context.redirect(
+                context.request.url.replace(host ?? '', redirectHostname)
+            );
+        } else {
+            return context.redirect(
+                context.request.url.replace(host ?? '', import.meta.env.PUBLIC_DOMAIN_NAME)
+            );
+        }
+    }
     return next();
 });
