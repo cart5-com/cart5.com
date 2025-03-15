835f1c521213a0ade12e50591412eb87b403f294
kerem <keremtiryaki@gmail.com>
2025-03-14 20:35:47 -0400

    chore: update environment configurations and streamline start scripts for improved deployment

--- apps/web-store-ssr-astro/src/middleware/websiteId.ts
+++ apps/web-store-ssr-astro/src/middleware/websiteId.ts
@@ -7,6 +7,16 @@ export const websiteIdMiddleware = defineMiddleware(async (context, next) => {
     console.log('websiteIdMiddleware IS_PROD', IS_PROD);
     console.log('websiteIdMiddleware PUBLIC_DOMAIN_NAME', import.meta.env.PUBLIC_DOMAIN_NAME);
     console.log('==============================================');
+    // print all env variables here
+    console.log('==============================================');
+    console.log('websiteIdMiddleware import.meta.env');
+    console.log(import.meta.env);
+    console.log('==============================================');
+    console.log('==============================================');
+    console.log('websiteIdMiddleware process.env');
+    console.log(process.env);
+    console.log('==============================================');
+
     const host = context.request.headers.get("Host");
     context.locals.WEBSITE = await getWebsiteByDefaultHostname(host ?? '');
     if (!context.locals.WEBSITE) {
