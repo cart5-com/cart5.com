33d0bd909a4607e6998e9b2167590ba27e02d1c6
kerem <keremtiryaki@gmail.com>
2025-03-19 15:00:50 -0400

    Enhance seed scripts to include team associations and streamline database migrations

--- packages/db/src/seed/startSeed.ts
+++ packages/db/src/seed/startSeed.ts
@@ -1,7 +1,12 @@
 import { IS_PROD } from "@lib/utils/getEnvVariable";
-import { createSeedUser } from "./createUser";
-// import db from "../drizzle"
-// import type { CloudflareObjectType } from "./cloudflareObjectType";
+import { createSeedUser } from "./createSeedUser";
+import {
+    addDomainToWebsite_Service,
+    createWebsite_Service
+} from "../services/website.service";
+import { getSupportTeamByHostname_Service } from "@db/services/team.service";
+import { addToTeamSeed } from "./addToTeamSeed";
+import { TEAM_PERMISSIONS } from "@lib/consts";
 
 
 const startSeed = async () => {
@@ -16,8 +21,34 @@ const startSeed = async () => {
     const name = "Moo";
     const profilePhotoUrl = "https://i.imgur.com/CUhzDlj_d.webp?maxwidth=760&fidelity=grand";
 
-    // const cfRaw: CloudflareObjectType = (await (await fetch("https://workers.cloudflare.com/cf.json")).json());
-    const user = await createSeedUser(email, password, name, profilePhotoUrl);
+
+    const cart5DevAdminUser = await createSeedUser(email, password, name, profilePhotoUrl);
+    const cart5DevWebsite = await createWebsite_Service(cart5DevAdminUser?.id!, "cart5dev", null);
+    await addDomainToWebsite_Service(cart5DevWebsite.id, cart5DevWebsite, "cart5dev.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(cart5DevWebsite.id, cart5DevWebsite, "www.cart5dev.com"); // default domain add as last one
+    const cart5Team_asSupportTeam = await getSupportTeamByHostname_Service("www.cart5dev.com")
+
+    const cuneytObite = await createSeedUser("cuneyt@obite.co.uk", password, "Cuneyt");
+    const obiteWebsite = await createWebsite_Service(cuneytObite?.id!, "obite", cart5Team_asSupportTeam?.teamId!);
+    await addDomainToWebsite_Service(obiteWebsite.id, obiteWebsite, "obite.cart5dev.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(obiteWebsite.id, obiteWebsite, "obite.co.uk"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(obiteWebsite.id, obiteWebsite, "obite.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(obiteWebsite.id, obiteWebsite, "www.obite.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(obiteWebsite.id, obiteWebsite, "www.obite.co.uk"); // default domain
+
+    const thushObite = await createSeedUser("thush@obite.co.uk", password, "thush");
+    await addToTeamSeed(obiteWebsite.ownerTeamId, thushObite?.id!, [TEAM_PERMISSIONS.FULL_ACCESS]);
+
+    //flames.obite.com created by thush inside obite website
+    const obiteTeam_asSupportTeam = await getSupportTeamByHostname_Service("www.obite.co.uk")
+    const flamesWebsite = await createWebsite_Service(thushObite?.id!, "flames", obiteTeam_asSupportTeam?.teamId!);
+    await addDomainToWebsite_Service(flamesWebsite.id, flamesWebsite, "flames.obite.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(flamesWebsite.id, flamesWebsite, "flames.obite.co.uk"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(flamesWebsite.id, flamesWebsite, "flames.com"); // secondary domain to redirect to default domain
+    await addDomainToWebsite_Service(flamesWebsite.id, flamesWebsite, "www.flames.com"); // default domain
+
+    const flamesRestaurantAdminUser = await createSeedUser("admin@flames.com", password, "Flames Admin");
+    await addToTeamSeed(flamesWebsite.ownerTeamId, flamesRestaurantAdminUser?.id!, [TEAM_PERMISSIONS.FULL_ACCESS]);
 }
 
 
