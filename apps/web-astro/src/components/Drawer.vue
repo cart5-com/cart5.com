c8374e47c00bd9693df8e66ecdc107f20616ec71
kerem <keremtiryaki@gmail.com>
2025-03-25 23:51:10 -0400

    Add drawer component and integrate dark mode toggle; update styles for restaurant card

---
+++ apps/web-astro/src/components/Drawer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { onMounted } from "vue";
+import DarkModeToggle from "@/ui-plus/DarkModeToggle.vue";
+
+onMounted(() => {
+    console.log("Drawer mounted");
+})
+
+</script>
+
+<template>
+    <div>
+        <DarkModeToggle />
+    </div>
+</template>
