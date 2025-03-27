d781a64c76970592890961abf63b4ddd8a78dcc1
kerem <keremtiryaki@gmail.com>
2025-03-27 00:04:42 -0400

    Fix item title retrieval in showItemModal to ensure correct display

--- apps/web-astro/src/components/restaurant/menu/showItemModal.ts
+++ apps/web-astro/src/components/restaurant/menu/showItemModal.ts
@@ -4,10 +4,8 @@ import ItemModal from './item/ItemModal.vue';
 const dialog = useDialog();
 
 export function showItemModal(itemId: ItemId, cartItem?: CartItem) {
-    const currentItem = window.menuRoot.allItems?.[itemId];
     dialog.show<CartItem>({
-        // title: menuRoot.value.allItems?.[itemId]?.itemLabel,
-        title: currentItem?.lbl,
+        title: window.menuRoot.allItems?.[itemId]?.lbl,
         component: ItemModal,
         dialogContentClass: "min-w-full md:min-w-[60vw] lg:min-w-[40vw] top-12 absolute",
         // dialogContentClass: "flex h-full min-h-full min-w-full flex-col p-0 md:h-[70vh] md:min-h-[70vh] md:min-w-[60vw] lg:min-w-[40vw]",
