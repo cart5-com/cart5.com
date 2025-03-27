f124c648c8d6b90b6dca4eb3530edc22dc6f4db3
kerem <keremtiryaki@gmail.com>
2025-03-27 19:29:50 -0400

    Enhance CartView component by adding a fallback key for v-for loop; update BaseLayout to include a slot with a loop for rendering elements.

--- apps/web-astro/src/components/restaurant/menu/item/CartView.vue
+++ apps/web-astro/src/components/restaurant/menu/item/CartView.vue
@@ -2,6 +2,8 @@
 import { generateCartItemTextSummary } from '@lib/types/menuType';
 import { cartStore } from './Cart.Store';
 import { showItemModal } from './showItemModal';
+import { Button } from '@/components/ui/button';
+
 const menuRoot = window.menuRoot;
 
 </script>
@@ -11,7 +13,7 @@ const menuRoot = window.menuRoot;
         <h1>Cart</h1>
         <ul>
             <li v-for="(cartItem, index) in cartStore"
-                :key="cartItem.itemId"
+                :key="cartItem.itemId ?? index"
                 class="cursor-pointer">
                 <pre @click="showItemModal(cartItem.itemId!, cartItem, index)"
                      class="text-xs max-w-full overflow-auto">{{ generateCartItemTextSummary(cartItem, menuRoot) }}
