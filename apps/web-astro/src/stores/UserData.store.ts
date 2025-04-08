8a3fe9773813d632d3345a29d63a3e5bf52eedbe
kerem <keremtiryaki@gmail.com>
2025-04-07 23:20:58 -0400

    Remove outdated TODO comment regarding bot browser user data loading

--- apps/web-astro/src/stores/UserData.store.ts
+++ apps/web-astro/src/stores/UserData.store.ts
@@ -150,7 +150,6 @@ const mergedUserData = (
     return deepMerge(anonUserData, formattedServerData);
 }
 
-// TODO: if bot browser, do not get userdata with api: authGlobalApiClient.get_user_data
 const loadUserData = async () => {
     if (import.meta.env.SSR) return;
     const isAfterLogin = typeof window !== 'undefined' && window.location.hash === '#after-login';
