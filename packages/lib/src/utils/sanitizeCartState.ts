e1fcd3339895c3692956b72d5bbb338cb18e46c5
kerem <keremtiryaki@gmail.com>
2025-05-01 02:36:14 -0400

    Add TODO comment to sanitizeCartState for state validation with menu data limits

--- packages/lib/src/utils/sanitizeCartState.ts
+++ packages/lib/src/utils/sanitizeCartState.ts
@@ -8,6 +8,7 @@ export const sanitizeCartState = (
     cartItem: CartItem,
     menuRoot: MenuRoot
 ): CartItem => {
+    // TODO: make sure all state checked with the menudata limits and maxs
     console.log('sanitizeCartState', cartItem, menuRoot);
     let cartItemState = cartItem;
     return cartItemState;
