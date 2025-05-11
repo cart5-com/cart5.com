97b8be498fccca7346c18fe61b6c63ec8c633d9c
kerem <keremtiryaki@gmail.com>
2025-05-11 14:28:05 -0400

    Refactor restartApp function to streamline Windows reload logic and remove unnecessary commented-out code

--- apps/orders-spa-vue/src/autoprint/utils/restartApp.ts
+++ apps/orders-spa-vue/src/autoprint/utils/restartApp.ts
@@ -5,18 +5,17 @@ export const restartApp = async () => {
     if (currentPrintTasks.value.length === 0) {
         if (typeof global === "undefined") {
             window.location.reload();
-            // } else if (process.platform === "win32") {
-            // it seems unnecessary, nw.Window.get().reload(); can handle this
-            //     var child = require("child_process").spawn(
-            //         process.execPath,
-            //         require('nw.gui').App.argv, {
-            //         cwd: require('path').dirname(process.cwd()),
-            //         detached: true,
-            //         stdio: "inherit"
-            //     });
-            //     //Don't wait for it
-            //     child.unref();
-            //     require('nw.gui').App.quit();
+        } else if (process.platform === "win32") {
+            var child = require("child_process").spawn(
+                process.execPath,
+                require('nw.gui').App.argv, {
+                cwd: require('path').dirname(process.cwd()),
+                detached: true,
+                stdio: "inherit"
+            });
+            //Don't wait for it
+            child.unref();
+            require('nw.gui').App.quit();
         } else {
             nw.Window.get().reload();
         }
