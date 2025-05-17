1d735e623e4046ff29fc31bef768708001aa272c
kerem <keremtiryaki@gmail.com>
2025-05-17 00:36:10 -0400

    Update PM2 configuration: set instances to 0 and enable cluster mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -37,14 +37,14 @@ module.exports = {
             cwd: "./apps/web-astro",
             script: "dotenvx run -f .env.production -- node ./dist/server/entry.mjs",
             // https://www.digitalocean.com/community/tutorials/how-to-scale-node-js-applications-with-clustering#step-5-using-pm2-for-clustering
-            // instances: 1,
-            // exec_mode: "cluster",
+            instances: 0,
+            exec_mode: "cluster",
             max_memory_restart: '150M',
             kill_timeout: 5000,
-            env: {
-                HOST: "0.0.0.0",
-                PORT: 3002
-            },
+            // env: {
+            //     HOST: "0.0.0.0",
+            //     PORT: 3002
+            // },
             // wait_ready: true, does not work with astro I could not find where to make `process.send('ready');`
         }
     ]
