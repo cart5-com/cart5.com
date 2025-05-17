7e607675037a798d5b888605bc6f4d59f228e15e
kerem <keremtiryaki@gmail.com>
2025-05-17 00:29:34 -0400

    Add dotenvx as a dev dependency and clean up start script

--- start.sh
+++ start.sh
@@ -4,9 +4,3 @@ pm2 reload ./ecosystem.config.cjs --update-env && pm2 save # Claude ai said star
 # pm2 startOrRestart ./ecosystem.config.cjs --update-env && pm2 save
 
 cd apps/proxy-caddy && pnpm prod:cache:purge
-
-cd ../../
-
-ls -la
-
-pwd
